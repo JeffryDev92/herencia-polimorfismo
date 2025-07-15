a52a0beba05c8659c7e715d6c20d2fb66ca104d0
JeffryDev92 <jeffry.lopezadev@gmail.com>
2025-07-15 00:54:34 -0500

    "feat: clase Main con prueba de polimorfismo entre Vehiculo, Automovil y Camion"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Vehiculo v1 = new Automovil("Toyota", "Corolla", 2020, 4);
+        Vehiculo v2 = new Camion("Volvo", "FH16", 2019, 18.0);
+
+        System.out.println("Demostración de polimorfismo:");
+        System.out.println("--------------------------------");
+        v1.mostrarInfo();  // Llama al método de Automovil
+        System.out.println("--------------------------------");
+        v2.mostrarInfo();  // Llama al método de Camion
     }
 }
